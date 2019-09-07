89a8c44716275bce3cbf5eaf768a6243f01104fc
ycj <seeyou_y@qq.com>
2019-09-07 21:30:13 +0800

    整理

--- src/main/java/com/example/DemoApp.java
+++ src/main/java/com/example/DemoApp.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+
+     //lalalalala
+
+        SpringApplication.run(DemoApp.class, args);
     }
 
 }
--- src/main/java/com/example/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.dao;
-
-import com.example.entity.User;
-
-import java.util.List;
-
-public interface UserDAO {
-    List<User> findAll();
-}
