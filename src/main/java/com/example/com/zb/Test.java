f0daa31a7a15d0c99db26e03188898c53b8164b0
Zhang <bb5581731@163.com>
2019-09-07 20:55:09 +0800

    zb add and sout

--- src/main/java/com/example/com/zb/Test.java
+++ src/main/java/com/example/com/zb/Test.java
@@ -2,6 +2,6 @@ package com.example.com.zb;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test zb");
     }
 }
